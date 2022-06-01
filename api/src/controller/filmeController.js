7ea3e387ac8b2f538b5d0f1d0b7b22fda727f4ae
Giuseppe Pallazini <Pepeteste1883@gmail.com>
2022-06-01 08:30:07 -0300

    Parte Final

--- api/src/controller/filmeController.js
+++ api/src/controller/filmeController.js
@@ -1,10 +1,10 @@
-import { addFilme, alterarImagem, listarPorId, listarTodosFilmes } from "../repository/filmeRepository.js"
+import { addFilme, alterarImagem, atlerarFilme, excluirFilme, listarPorId, listarPorNome, listarTodosFilmes } from "../repository/filmeRepository.js"
 
 import multer from 'multer'
 import { Router } from "express"
 
 const server = Router();
-const upload = multer( { dest:'storage/capaFilmes' });
+const upload = multer({ dest: 'storage/capaFilmes' });
 
 server.post('/filme', async (req, resp) => {
     try {
@@ -22,7 +22,7 @@ server.post('/filme', async (req, resp) => {
         if (!inserirFilme.lancamento)
             throw new Error('Lançamento obrigatório!')
 
-        if (!inserirFilme.disponivel)
+        if (inserirFilme.disponivel == undefined)
             throw new Error('Disponibilidade obrigatória!')
 
         if (!inserirFilme.usuario)
@@ -38,20 +38,20 @@ server.post('/filme', async (req, resp) => {
 });
 
 
-server.put('/filme/:id/capa', upload.single('capa'), async (req, resp) =>{
+server.put('/filme/:id/capa', upload.single('capa'), async (req, resp) => {
     try {
         const { id } = req.params;
         const imagem = req.file.path
 
         const resposta = await alterarImagem(imagem, id)
-        if(resposta != 1){
+        if (resposta != 1) {
             throw new Error('A imagem não pode ser salva')
         }
         resp.status(204).send();
     } catch (err) {
         resp.status(400).send({
             erro: err.message
-        })        
+        })
     }
 })
 
@@ -67,22 +67,93 @@ server.get('/filme', async (req, resp) => {
     }
 })
 
-
-server.get('/filme/:id', async (req, resp) => {
+server.get('/filme/busca', async (req, resp) => {
     try {
-        const id  = Number(req.params.id)
-        const resposta = await listarPorId(id)
+        const { nome } = req.query
+        const resposta = await listarPorNome(nome)
 
-        if(!resposta)
+        if (resposta.length == 0)
             resp.status(404).send([])
         else
-            resp.status(resposta)
-        resp.send(resposta);
+            resp.send(resposta);
     } catch (err) {
         resp.status(400).send({
             erro: err.message
         })
     }
+})
+
+server.get('/filme/:id', async (req, resp) => {
+    try {
+        const id = Number(req.params.id)
+        const resposta = await listarPorId(id)
+
+        if (!resposta)
+            resp.status(404).send([])
+        else
+            resp.send(resposta);
+    } catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+server.delete('/filme/:id', async (req, resp) => {
+    try {
+        const { id } = req.params;
+        const resposta = await excluirFilme(id)
+        if (resposta != 1)
+            throw new Error('Filme não pode ser removido')
+
+        resp.status(204).send();
+
+    } catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+
+server.put('/filme/:id', async (req, resp) => {
+    try {
+        const { id } = req.params
+        const filme = req.body
+
+        if (!filme.nome)
+            throw new Error('Nome obrigatório!')
+
+        if (!filme.sinopse)
+            throw new Error('Sinopse obrigatória!')
+
+        if (filme.avaliacao == undefined || filme.avaliacao < 0)
+            throw new Error('Avaliação obrigatória!')
+
+        if (!filme.lancamento)
+            throw new Error('Lançamento obrigatório!')
+
+        if (filme.disponivel == undefined )
+            throw new Error('Disponibilidade obrigatória!')
+
+        if (!filme.usuario)
+            throw new Error('Usuário não logado!')
+
+
+        const resposta = await atlerarFilme(id, filme)
+        if(resposta != 1)
+            throw new Error('Filme não pode ser alterado!')
+
+        else 
+            resp.status(204).send();
+    } catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+
+
+
 })
 
 export default server;
--- api/src/repository/filmeRepository.js
+++ api/src/repository/filmeRepository.js
@@ -16,23 +16,24 @@ export async function addFilme(filme) {
 
 
 export async function alterarImagem(imagem, id) {
-    const comando = 
-    ` UPDATE tb_filme 
-    SET img_filme     = ?
-  WHERE id_filme = ? `;
-    
-  const [resposta] = await con.query(comando, [imagem, id]);
-  return resposta.affectedRows;
+    const comando =
+        ` UPDATE tb_filme 
+    SET      img_filme      = ?
+  WHERE      id_filme       = ? `;
+
+    const [resposta] = await con.query(comando, [imagem, id]);
+    return resposta.affectedRows;
 }
 
 
 export async function listarTodosFilmes() {
-    const comando = 
-    `SELECT id_filme			id,
+    const comando =
+        `SELECT id_filme			id,
             nm_filme			nome,
             vl_avaliacao		avaliacao,
             dt_lancamento	    lancamento,
-            bt_disponivel	    disponivel
+            bt_disponivel	    disponivel,
+            id_usuario          usuario
        FROM tb_filme`;
 
     const [linhas] = await con.query(comando);
@@ -41,15 +42,60 @@ export async function listarTodosFilmes() {
 
 
 export async function listarPorId(id) {
-    const comando = 
-    `SELECT id_filme			id,
+    const comando =
+        `SELECT id_filme			id,
             nm_filme			nome,
+            ds_sinopse          sinopse,
+            img_filme           imagem,
             vl_avaliacao		avaliacao,
             dt_lancamento	    lancamento,
-            bt_disponivel	    disponivel
+            bt_disponivel	    disponivel,
+            id_usuario          usuario
        FROM tb_filme
        WHERE id_filme = ? `;
 
     const [linhas] = await con.query(comando, [id]);
     return linhas[0];
+}
+
+
+export async function listarPorNome(nome) {
+    const comando =
+        `SELECT id_filme			id,
+            nm_filme			nome,
+            vl_avaliacao		avaliacao,
+            dt_lancamento	    lancamento,
+            bt_disponivel	    disponivel,
+            id_usuario          usuario
+       FROM tb_filme
+       WHERE nm_filme like ? `;
+
+    const [linhas] = await con.query(comando, [`%${nome}%`]);
+    return linhas;
+}
+export async function excluirFilme(id) {
+    const comando =
+        `
+    DELETE FROM tb_filme 
+      WHERE id_filme = ?
+    `;
+    const [resposta] = await con.query(comando, [id]);
+    return resposta.affectedRows;
+
+}
+
+
+export async function atlerarFilme(id, filme) {
+    const comando =
+    `UPDATE tb_filme 
+    SET     nm_filme      = ?,
+            ds_sinopse    = ?,
+            vl_avaliacao  = ?,
+            dt_lancamento = ?,
+            bt_disponivel = ?,
+            id_usuario    = ?
+    WHERE   id_filme      = ? `;
+
+    const [resposta] = await con.query(comando, [filme.nome, filme.sinopse, filme.avaliacao, filme.lancamento, filme.disponivel, filme.usuario, id]);
+    return resposta.affectedRows;
 }
